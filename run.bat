719df3731bfd8f3d40fb0238e9fa06533af1522e
emilyewesel <ewesel@stanford.edu>
2023-05-20 12:13:08 -0700

    more gpu

--- run.bat
+++ run.bat
@@ -2,8 +2,8 @@
 #SBATCH --job-name=train_depressive_symptoms_only_tabular
 #SBATCH --time=14:00:00
 #SBATCH -p kpohl
-#SBATCH --gpus 1
-#SBATCH -c 10
+#SBATCH --gpus 12
+#SBATCH -c 30
 #SBATCH --mail-type=BEGIN,END,FAIL
 
 ml python/3.9.0
