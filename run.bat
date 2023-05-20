38b07e227c525bb4c24b6ff6381aa2a83cc95f9a
emilyewesel <ewesel@stanford.edu>
2023-05-20 11:42:37 -0700

    Update run.bat

--- run.bat
+++ run.bat
@@ -2,8 +2,8 @@
 #SBATCH --job-name=train_depressive_symptoms_only_tabular
 #SBATCH --time=14:00:00
 #SBATCH -p kpohl
-#SBATCH --gpus 3
-#SBATCH -c 20
+#SBATCH --gpus 1
+#SBATCH -c 10
 #SBATCH --mail-type=BEGIN,END,FAIL
 
 ml python/3.9.0
