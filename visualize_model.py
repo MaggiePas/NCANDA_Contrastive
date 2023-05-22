ff676ab87fef8e2033162c924ab453d4b0131a6a
tulika-jha <jtulika2017@gmail.com>
2023-05-21 19:31:55 -0700

    corrections

--- visualize_model.py
+++ visualize_model.py
@@ -14,10 +14,11 @@ print(model.scaler)
 # load val data
 data = NCANDADataModule()
 data.prepare_data()
-val_loader = data.val_dataloader()
+#val_loader = data.val_dataloader()
+val_data = data.validation
 
 # predict with the model
-y_hat = model(val_loader)
+y_hat = model(val_data)
 
 #
 # # load val data
