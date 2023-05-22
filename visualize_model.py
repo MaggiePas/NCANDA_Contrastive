7e4c9db18e4284761010a3c121bf418574283249
tulika-jha <jtulika2017@gmail.com>
2023-05-21 23:46:22 -0700

    add medcam

--- visualize_model.py
+++ visualize_model.py
@@ -18,6 +18,16 @@ model = medcam.inject(model, output_dir="attention_maps", save_maps=True)
 # set in evaluation mode (no grads)
 model.eval()
 
+data = NCANDADataModule()
+data.prepare_data()
+val_loader = data.val_dataloader()
+
+for batch in val_loader:
+    # Every time forward is called, attention maps will be generated and saved in the directory "attention_maps"
+    output = model(batch)
+
+
+"""
 image_path = "/home/groups/kpohl/ncanda-multi-modal/T1/NCANDA_S00083.nii.gz"
 image = nib.load(image_path)
 image = image.get_fdata()
@@ -26,7 +36,6 @@ image = np.array(image, dtype=np.float32)
 image = image / image.max()
 image = resize(image, (IMAGE_SIZE, IMAGE_SIZE, IMAGE_SIZE))
 
-
 image = torch.from_numpy(image)
 image = image.reshape((1, IMAGE_SIZE, IMAGE_SIZE, IMAGE_SIZE))
 print("after converting to tensor, image shape is : {}".format(image.shape))
@@ -34,5 +43,6 @@ print("after converting to tensor, image shape is : {}".format(image.shape))
 # predict with the model
 y_hat = model(image, tab=None)
 print(y_hat)
+"""
 
 
