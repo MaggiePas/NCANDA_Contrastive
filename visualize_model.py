d734c48e1b1f19379f0a36740e0cec93e1bde310
tulika-jha <jtulika2017@gmail.com>
2023-05-21 22:57:37 -0700

    add medcam

--- visualize_model.py
+++ visualize_model.py
@@ -5,20 +5,18 @@ import numpy as np
 import torch
 from multimodal_dataset import resize
 from settings import IMAGE_SIZE
+import medcam
 
 
 checkpoint_path = "/home/users/tulikaj/NCANDA_Contrastive/lightning_logs/lxf2jrac/checkpoints/epoch=0-step=70.ckpt"
 
 model = MultiModModelSwinEnc.load_from_checkpoint(checkpoint_path)
 
+# Inject model with M3d-CAM
+model = medcam.inject(model, output_dir="attention_maps", save_maps=True)
+
 # set in evaluation mode (no grads)
 model.eval()
-
-# # load val data
-# data = NCANDADataModule()
-# data.prepare_data()
-# #val_loader = data.val_dataloader()
-# val_data = data.validation
 
 image_path = "/home/groups/kpohl/ncanda-multi-modal/T1/NCANDA_S00083.nii.gz"
 image = nib.load(image_path)
@@ -28,6 +26,7 @@ image = np.array(image, dtype=np.float32)
 image = image / image.max()
 image = resize(image, (IMAGE_SIZE, IMAGE_SIZE, IMAGE_SIZE))
 
+
 image = torch.from_numpy(image)
 image = image.reshape((1, IMAGE_SIZE, IMAGE_SIZE, IMAGE_SIZE))
 print("after converting to tensor, image shape is : {}".format(image.shape))
@@ -36,12 +35,4 @@ print("after converting to tensor, image shape is : {}".format(image.shape))
 y_hat = model(image, tab=None)
 print(y_hat)
 
-#
-# # load val data
-# data = NCANDADataModule()
-# data.prepare_data()
-# val_loader = data.val_dataloader()
-#
-#
-# # predict with model
-#y_hat = model()
+
