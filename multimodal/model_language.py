8a1b76d444e45039276ccb32382e2d947e64e54a
emilyewesel <ewesel@stanford.edu>
2023-06-01 23:04:20 -0700

    Update model_language.py
    No imaging data

--- multimodal/model_language.py
+++ multimodal/model_language.py
@@ -33,8 +33,8 @@ class MultiModModelWithLanguage(LightningModule):
                                num_classes=240, # might change this one
                                n_input_channels=1
                                )
-        base = 'michiyasunaga/BioLinkBERT-base'
-        # base = "bert-base-uncased"
+        # base = 'michiyasunaga/BioLinkBERT-base'
+        base = "bert-base-uncased"
 
         # qa_pipeline = pipeline("question-answering", model="medalpaca/medalpaca-7b", tokenizer="medalpaca/medalpaca-7b")
         # question = "What are the symptoms of diabetes?"
@@ -183,7 +183,8 @@ class MultiModModelWithLanguage(LightningModule):
         language_features_compressed = self.language_fc(pooled_states)
 
         # concat image, tabular data and data from language model
-        x = torch.cat((img, tab_without_age_sex, language_features_compressed), dim=1)
+        #img
+        x = torch.cat((tab_without_age_sex, language_features_compressed), dim=1)
 
         x = F.relu(self.fc2(x))
 
