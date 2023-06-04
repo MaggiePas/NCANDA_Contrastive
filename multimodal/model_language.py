f6b1e09fae64688c00efc1b6556c0cd4db9790ab
emilyewesel <ewesel@stanford.edu>
2023-06-04 02:09:27 -0700

    fiji water
    distilled

--- multimodal/model_language.py
+++ multimodal/model_language.py
@@ -37,7 +37,7 @@ class MultiModModelWithLanguage(LightningModule):
         base = 'michiyasunaga/BioLinkBERT-base'
         base = "roberta-base"
         base = "distilbert-base-uncased"
-        base = "bert-base-uncased"
+        #base = "bert-base-uncased"
 
         # qa_pipeline = pipeline("question-answering", model="medalpaca/medalpaca-7b", tokenizer="medalpaca/medalpaca-7b")
         # question = "What are the symptoms of diabetes?"
