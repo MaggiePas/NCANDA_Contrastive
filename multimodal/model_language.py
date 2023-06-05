a4c91a045a53c46abbc1798aa0b192c094d08ba2
emilyewesel <ewesel@stanford.edu>
2023-06-04 20:38:56 -0700

    Update model_language.py

--- multimodal/model_language.py
+++ multimodal/model_language.py
@@ -621,7 +621,7 @@ class MultiModModelWithLanguage(LightningModule):
         max_length = min(len(string) for string in batch_sentences)
         padded_strings = []
         for string in batch_sentences:
-            truncated_string = string#[:1000]
+            truncated_string = string[:400]
         #     padded_string = truncated_string.ljust(max_length, " ")
             padded_strings.append(truncated_string)
         return padded_strings
