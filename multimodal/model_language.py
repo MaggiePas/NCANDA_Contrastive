78a05b1e2a75da07e624689323ef6c57e3e7a214
emilyewesel <ewesel@stanford.edu>
2023-06-06 16:32:42 -0700

    Update model_language.py

--- multimodal/model_language.py
+++ multimodal/model_language.py
@@ -39,7 +39,7 @@ class MultiModModelWithLanguage(LightningModule):
         # base = "roberta-base"
         # base = "distil-bert-base-uncased"
         # base = "bert-base-uncased"
-        base = "distilroberta-base"
+        # base = "distilroberta-base"
 
         # qa_pipeline = pipeline("question-answering", model="medalpaca/medalpaca-7b", tokenizer="medalpaca/medalpaca-7b")
         # question = "What are the symptoms of diabetes?"
