e61e7dedaebbef83b42aa7700029c3b86816c837
Tulika Jha <tulikaj@sh03-13n05.int>
2023-06-07 02:05:18 -0700

    corrections

--- saliency_maps.py
+++ saliency_maps.py
@@ -9,27 +9,29 @@ from multimodal.model import MultiModModel
 import matplotlib.pyplot as plt
 
 
-checkpoint_folder = "swin_UNETR_12_cross_corr_128ImgSize"
-checkpoint_file = "epoch=19-step=8480.ckpt"
+#checkpoint_folder = "swin_UNETR_12_cross_corr_128ImgSize"
+checkpoint_folder  = "e48qyy41"
+checkpoint_file = "epoch=0-step=424.ckpt"
 checkpoint_path = f'/home/users/tulikaj/NCANDA_Contrastive/lightning_logs/{checkpoint_folder}/checkpoints/{checkpoint_file}'
 layer_name = "swin_enc"
 output_dir = f"attention_maps/{checkpoint_folder}/"
 store_png_here = "final_pngs"
 
 
-def saliency(img, model):
+def saliency(img,tab, model):
     for param in model.parameters():
         param.requires_grad = False
     # set in evaluation mode (no grads)
     model.eval()
     # set input grad to true
     img.requires_grad = True
-    preds = model(img)
-    score, indices = torch.max(preds, 1)
+    pred = model(img,tab)
+    score = pred
     # backward pass to get gradients of score predicted class w.r.t. input image
     score.backward()
     # get max along channel axis
-    slc, _ = img.grad[1]
+    print(f"shape of img grad:{img.grad.shape}")
+    slc = img.grad[0]
     # normalize to [0..1]
     slc = (slc - slc.min()) / (slc.max() - slc.min())
     # plot image and its saliency map in
@@ -46,6 +48,6 @@ data.prepare_data()
 val_loader = data.val_dataloader()
 
 for batch in val_loader:
-    slc = saliency(batch[0], model)
+    slc = saliency(batch[0], batch[1], model)
     print(type(slc), slc.shape)
     break
