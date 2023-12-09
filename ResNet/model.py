63bdf021332a393ea20ebd00089d5336ce9a6634
emilyewesel <ewesel@stanford.edu>
2023-12-08 20:10:05 -0800

    Update model.py

--- ResNet/model.py
+++ ResNet/model.py
@@ -22,7 +22,7 @@ class ResNetModel(LightningModule):
         self.class_weights = class_weights
 
         self.net = nn.Sequential(
-            DenseNet(num_classes=2, in_channels=1),
+            DenseNet(spatial_dims=3, in_channels=1, out_channels=2),
             # densenet121(pretrained=False, spatial_dims=3, n_input_channels=1, num_classes=2),
             # resnet10(pretrained=False, spatial_dims=3, n_input_channels=1, num_classes=2),
             nn.Dropout(0.2)  # Adjust the dropout rate as needed
